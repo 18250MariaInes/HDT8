c4ed5488f23638e972425a8e8d58bdb16c9bad81
CamilaGO <gon18398@uvg.edu.gt>
2019-04-01 20:21:23 -0600

    Implementacion del main con VectorHeap de Pacientes

--- HDT8/src/hdt8/HDT8.java
+++ HDT8/src/hdt8/HDT8.java
@@ -5,6 +5,15 @@
  */
 package hdt8;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import static java.util.Collections.list;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 /**
  *
  * @author maria
@@ -16,6 +25,45 @@ public class HDT8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList();
+        VectorHeap<Paciente> heaPacientes = new VectorHeap<>();
+        
+        //Se lee el txt y se guarda cada linea en un array list
+        try{
+            Stream<String> lines = Files.lines(
+                    Paths.get("pacientes.txt"),
+                    StandardCharsets.UTF_8
+            );
+            
+            lines.forEach(a -> list.add(a));
+        } catch (IOException e){
+            System.out.println("Error!");
+        }
+        
+        
+        //Guarda cada paciente en el heap
+        for (int i = 0; i < list.size(); i++){
+            String paci = (list.get(i));
+            System.out.print(paci + "\n");
+            String[] datosPaci = paci.split(", ");
+            System.out.print(datosPaci[0]+ "\n");
+            System.out.print(datosPaci[1]+ "\n");
+            System.out.print(datosPaci[2]+ "\n");
+            Paciente paciente = new Paciente(datosPaci[0],datosPaci[1],datosPaci[2]);
+            heaPacientes.add(paciente);
+        }
+        
+        //Imprime el heap ya con los pacientes ordenado segun la prioridad
+        System.out.print("\n Impresion del heap \n");
+        int size = heaPacientes.size();
+        int contador =1;    
+        for (int c = 0;c<size;c++){
+            System.out.println(heaPacientes.getFirst().toString());
+            heaPacientes.remove();
+        }
+        
+      
     }
     
 }
