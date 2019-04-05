891234f29184bc99f63c1af70503a12e00ad3c65
CamilaGO <gon18398@uvg.edu.gt>
2019-04-05 09:40:33 -0600

    javadoc y pruebas Junit

---
+++ HDT8/test/VectorHeapTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Caimila.MariaInes
+ */
+public class VectorHeapTest {
+    
+    public VectorHeapTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class VectorHeap.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        VectorHeap<Paciente> instance = new VectorHeap<>();
+        Paciente paciente = new Paciente("Enrique","alergia","B");//Object value = null;
+        //VectorHeap instance = new VectorHeap();
+        instance.add(paciente);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    /**
+     * Test of remove method, of class VectorHeap.
+     */
+    @Test
+    public void testRemove() {
+        System.out.println("remove");
+        VectorHeap<Paciente> instance = new VectorHeap<>();
+        Paciente paciente = new Paciente("Enrique","alergia","B");//Object value = null;
+        instance.add(paciente);
+        instance.remove();
+    }
+
+        
+}
